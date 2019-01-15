1e7f3ee0fbed73b5c9914dde9eb260a6f2bf156a
Matthews <Drew.Matthews@coxautoinc.com>
2019-01-15 13:26:12 -0500

    Added Form Layout

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -2,13 +2,15 @@ import  React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import {Col, Row, Container, Nav, NavItem, NavLink} from "reactstrap";
 import StringSubTool from "./Components/stringSubTool";
+import "./Components/sidenav.css";
+
 
 const AppRouter = () => (
 	<Router>
 		<Container fluid>
 			<Row noGutters>
-				<Col sm="2">
-					<Nav vertical>
+				<Col sm="2" >
+					<Nav vertical className="sidenav">
 						<NavItem>
 							<NavLink>
 								<Link to="/">Home</Link>
@@ -19,9 +21,7 @@ const AppRouter = () => (
 				<Col sm="10">
 						<Route path="/" exact component={StringSubTool} />			
 				</Col>
-	
 			</Row>
-	
 		</Container>
 	</Router>
   
--- src/Components/stringSubTool.js
+++ src/Components/stringSubTool.js
@@ -1,14 +1,63 @@
 import React, { Component } from 'react';
 import './stringSubTool.css';
+import './utils.css';
+import {Container, Row, Col, Label,Button, Collapse, Card, CardBody, CardHeader, Form, Input, FormGroup} from 'reactstrap'
 
 class StringSubTool extends Component {
-  render() {
-    return (
-      <div className="string-sub-tool">
-        <p>hELLO</p>
-      </div>
-    );
-  }
+
+    constructor(props) {
+        super(props);
+        this.toggle = this.toggle.bind(this);
+        this.state = { collapse: false };
+    }
+
+    toggle() {
+        this.setState({ collapse: !this.state.collapse });
+    }
+
+    render() {
+        return (
+        <Container className="string-sub-tool text-left">
+            <h2 className="tool-header">String Substitution Tool</h2>   
+            <Button color="primary" onClick={this.toggle} style={{ marginBottom: '1rem' }}>Help</Button>
+            <Collapse isOpen={this.state.collapse} className="mb-4">
+                <Card className="help-section border">
+                    <CardHeader tag="h2" className="color-primary">How To Use</CardHeader>
+                    <CardBody>This is example text will replace later</CardBody>  
+                </Card>
+            </Collapse>
+            <Form>
+                <Label for="listInput">List Input</Label>
+                <Input type="textarea" id="listInput"></Input>
+
+                <Label for="replaceSting">String to Substitute</Label>
+                <Input type="text" id="replaceSting"></Input>
+
+                <Label for="subSymbol">Symbol to Substitute</Label>
+                <Input type="text" id="subSymbol" defaultValue="###"></Input>
+
+                <Label for="trimString">Characters to Trim</Label>
+                <Input type="select" id="trimString">
+                    <option>-1</option>
+                    <option>-2</option>
+                    <option>-3</option>
+                    <option>-4</option>
+                </Input>
+                <FormGroup check>
+                    <Label check>
+                        <Input type="checkbox" /> To Lower
+                    </Label>
+                </FormGroup>
+                <FormGroup check>
+                    <Label check>
+                        <Input type="checkbox" /> Output on 1 line
+                    </Label>
+                </FormGroup>
+                <Button className="form-button" color="primary">Submit</Button>
+            </Form>
+        </Container>
+        );
+    }
 }
 
 export default StringSubTool;
