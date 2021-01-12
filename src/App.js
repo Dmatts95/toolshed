e89206a2edc0824e00be6cd9adcd78a21dd2e63b
Drew <chrisAMatthews95@gmail.com>
2021-01-11 19:47:12 -0500

    removed unused import

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import HeaderNav from './components/HeaderNav';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import RoutingInfo from './routing/routeConfig';
-//import { Button } from 'reactstrap';
-//import AppRouter from './AppRouter';
 
 const App = () => {
   return (
@@ -18,11 +16,6 @@ const App = () => {
         </Switch>
       </Router>
     </>
-
-
-    // <div className="App">
-    //   <HeaderNav />
-    // </div>
   )
 }
 
